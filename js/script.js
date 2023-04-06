5bca1be81c8a6fb715062d039ec29d3da6527888
razibhosen24 <90138277+razibhosen24@users.noreply.github.com>
2023-04-06 23:46:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// tooltip start
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// tooltip end 
